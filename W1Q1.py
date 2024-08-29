734bf2f94812234ea63893a644ab0ef4195d6c43
Mohd Ismaeel <pmohd367@gmail.com>
2024-08-29 20:04:15 +0530

    Delete W1Q1.py

--- W1Q1.py
+++
@@ -1,9 +0,0 @@
-num1 = int(input("Enter first number"))
-num2 = int(input("Enter second number"))
-
-prod = num1 * num2
-sum = num1 + num2
-if prod <= 5000:
-     print("The sum is ", sum)
-else:
-     print("The product is ", prod)
